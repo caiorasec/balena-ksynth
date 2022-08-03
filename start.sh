a3fe39cd69ce18c99bb594c573afcf6e25cece9f
Caio Silva <cainho.rasec@gmail.com>
2022-08-03 14:52:07 -0300

    Made a fix on the activation key display and descriptions improvement

--- start.sh
+++ start.sh
@@ -7,13 +7,17 @@ fi
 
 [ -z "$COMPANY_ID" ] && echo "Company ID not setting" || echo "Company ID ok: $COMPANY_ID"
 [ -z "$PROBE_NAME" ] && echo "Probe name not setting" || echo "Probe name ok: $PROBE_NAME"
-if [ -z "$KSYNTH_ID" ]; then
-    ksynth-agent --company-id "${COMPANY_ID}" --name "${PROBE_NAME}" --browser-path /usr/bin/chromium
-    echo "Save KSYNTH_ID: $(cat /ksynth.id)"
-    exit 0
+if [ -z "$KSYNTH_KEY" ]; then
+    ksynth-agent --company-id "${COMPANY_ID}" --name "${PROBE_NAME}" --browser-path /usr/bin/chromium &
+    sleep 10
+    while :; do
+        if [ -f /ksynth.id ]; then
+            echo "Save the key in balena's device variable KSYNTH_KEY: $(cat /ksynth.id)"
+        fi
+        sleep 4
+    done
 else
-    echo "KSYNTH_ID ok: $KSYNTH_ID"
-    echo "$KSYNTH_ID" > /ksynth.id
+    echo "Variable KSYNTH_KEY is OK: $KSYNTH_KEY"
+    echo "$KSYNTH_KEY" > /ksynth.id
+    ksynth-agent --company-id "${COMPANY_ID}" --name "${PROBE_NAME}" --browser-path /usr/bin/chromium
 fi
-
-ksynth-agent --company-id "${COMPANY_ID}" --name "${PROBE_NAME}" --browser-path /usr/bin/chromium
